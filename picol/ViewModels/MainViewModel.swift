93486441514f7e8e433bbb5519eeca652be012aa
famisics <dev@uiro.dev>
2024-12-14 22:25:59 +0900

    update キャラクターの動きを改善

--- picol/ViewModels/MainViewModel.swift
+++ picol/ViewModels/MainViewModel.swift
@@ -2,5 +2,14 @@ import SwiftUI
 
 class MainViewModel: ObservableObject {
     @Published var type: String = "Fire"
-    @Published var face: String = "Flash"
+    @Published var face: String = "Angry"
+    let faces = ["Fine", "Flash", "Sad", "", "Heart", "Angry"]
+
+    func changeType(type: String) {
+        self.type = type
+    }
+
+    func changeFace(face: String) {
+        self.face = face
+    }
 }
--- picol/Views/Components/PicolImage.swift
+++ picol/Views/Components/PicolImage.swift
@@ -9,15 +9,45 @@ struct PicolImage: View {
     @EnvironmentObject var viewModel: MainViewModel
     @State private var isSceneOne = true
     
+    var viewface: String {
+        viewModel.face == "Heart" ? "Fine" : viewModel.face == "Angry" ? "Sad" : viewModel.face
+    }
+    
+    var isHeart: Bool {
+        viewModel.face == "Heart"
+    }
+
+    var isAngry: Bool {
+        viewModel.face == "Angry"
+    }
+    
     var body: some View {
-        Image("Picol\(viewModel.type)\(viewModel.face)\(isSceneOne ? "1" : "2")")
-            .resizable()
-            .scaledToFit()
-            .onAppear {
-                Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
-                    let currentSecond = Calendar.current.component(.second, from: Date())
-                    isSceneOne = currentSecond % 2 == 0
+        ZStack {
+            Image("Picol\(viewModel.type)\(viewface)\(isSceneOne ? "1" : "2")")
+                .resizable()
+                .scaledToFit()
+                .onAppear {
+                    Timer.scheduledTimer(withTimeInterval: 0.8, repeats: true) {
+                        _ in
+                        isSceneOne.toggle()
+                    }
                 }
+            if isHeart {
+                Image("PicolEffectHeart\(isSceneOne ? "1" : "2")")
+                    .resizable()
+                    .scaledToFit()
             }
+            if isAngry {
+                Image("PicolEffectAngry")
+                    .resizable()
+                    .scaledToFit()
+            }
+        }
+    }
+}
+
+struct PicolImage_Previews: PreviewProvider {
+    static var previews: some View {
+        PicolImage().environmentObject(MainViewModel())
     }
 }
