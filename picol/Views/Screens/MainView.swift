78cc2fa26fbbc3408bc7078e6cec83b23738b6f1
famisics <dev@uiro.dev>
2024-12-14 14:42:22 +0900

    update メイン画面でキャラクターが動くように

--- picol/Views/Screens/MainView.swift
+++ picol/Views/Screens/MainView.swift
@@ -6,18 +6,26 @@
 import SwiftUI
 
 struct MainView: View {
+    @State private var isPicolFire1 = true
+    var type: String = "Picol"
+    var mode: String = "Fire"
+    
     var body: some View {
         VStack {
-            Image("PicolFire1")
+            Image("\(type)\(mode)\(isPicolFire1 ? "1" : "2")")
                 .resizable()
                 .scaledToFit()
-                .frame(width: 200, height: 200)
+                .frame(width: 300, height: 300)
                 .onTapGesture {
-                    print("PicolFire tapped")
+                    print("\(type)\(mode) tapped")
                 }
-                
         }
         .backgroundImage("MainBackground")
+        .onAppear {
+            Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+                isPicolFire1.toggle()
+            }
+        }
     }
 }
 
